cc2cde918f75a882a134b1538c0bd025b2569f6c
Big15 <15.js.tqar@gmail.com>
2016-05-05 16:08:07 +0100

    Added files via upload

--- GereVendas/src/Leitura.java
+++ GereVendas/src/Leitura.java
@@ -1,115 +1,40 @@
 
 import java.io.*;
+import static java.lang.System.out;
 import java.util.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author João
- */
 public class Leitura {
-    
 
-public static void print (TreeSet<String> cenas){
-    TreeSet<String> aux = cenas;
-   
-    for(String s: aux){
-    System.out.println(s);
-    }
-    System.out.println("Número Total de Elementos:");
-    System.out.println(aux.size());
+    public static void print(TreeSet<String> cenas) {
+        TreeSet<String> aux = cenas;
 
-}
-    
-public static TreeSet<String> lerProd(String file_path){
-    String path;
-    TreeSet<String> prod =  new TreeSet<String>();
-    path = file_path;
-    try{
-    BufferedReader reader = new BufferedReader(new FileReader(path));
-   
-    String cod;
-    while((cod=reader.readLine())!= null){
-    
-    prod.add(cod);
-    
+        for (String s : aux) {
+            System.out.println(s);
+        }
     }
-    return prod;
-    }catch (Exception e){
-    System.err.format("IOException: %s%n", e);
+
+    public static TreeSet<String> lerFiles(String file_path) {
+        String path;
+        TreeSet<String> prod = new TreeSet<String>();
+        path = file_path;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+
+            String cod;
+            while ((cod = reader.readLine()) != null) {
+                Crono.start();
+                prod.add(cod);
+                Crono.stop();
+            }
+        } catch (IOException e) {
+            out.println(e.getMessage());
             return null;
-    }
-
-    
-
-}
-/*
-public static TreeSet<String> lerProd2(String file_path){
-    String path;
-    String cod;
-    TreeSet<String> prod =  new TreeSet<String>();
-    path = file_path;
-      try {
-          Scanner input = new Scanner(path);
-       
-    while (input.hasNext()){
-        cod = Input.lerString();
-        prod.add(cod);
-    }    
-      
+        }
+        System.out.println("Ficheiro Vendas_3M (Buffer)");
+        System.out.println("Lidas e guardadas " + prod.size() + " linhas");
+        System.out.println("Tempo: " + Crono.print() + " segundos");
         return prod;
-      }catch (Exception e) {
-          System.err.format("IOException: %s%n", e);
-            return null;
-    }
-    
-
-}*/
-
-public static TreeSet<String> lerClie(String file_path){
-    String path;
-    TreeSet<String> clie =  new TreeSet<String>();
-    path = file_path;
-    try{
-    BufferedReader reader = new BufferedReader(new FileReader(path));
-    String cod;
-    while((cod=reader.readLine())!= null){
-    
-       clie.add(cod);
-    
-    }
-    return clie;
-    }catch (Exception e){
-    System.err.format("IOException: %s%n", e);
-            return null;
     }
 
-}
-
-public static TreeSet<String> lerVen(String file_path){
-    String path;
-    TreeSet<String> ven =  new TreeSet<String>();
-    path = file_path;
-    try{
-    BufferedReader reader = new BufferedReader(new FileReader(path));
-    String cod;
-    while((cod=reader.readLine())!= null){
-    
-    ven.add(cod);
-    
-    }
-    return ven;
-    }catch (Exception e){
-    System.err.format("IOException: %s%n", e);
-            return null;
-    }
-
-}
-    
 }
   
---
+++ GereVendas/src/Venda.java
@@ -0,0 +1,146 @@
+
+import java.util.*;
+
+public class Venda {
+
+    private static String codP;
+    private static String codC;
+    private static double preco;
+    private static int filial;
+    private static int uni;
+    private static int mes;
+    private static String promo;
+
+    public Venda(String codP, String codC, float preco, int filial, int uni, int mes, String p) {
+        this.codP = codP;
+        this.codC = codC;
+        this.preco = preco;
+        this.filial = filial;
+        this.uni = uni;
+        this.mes = mes;
+        this.promo = p;
+    }
+
+    public Venda() {
+        this.codP = new String();
+        this.codC = new String();
+        this.preco = 0.0;
+        this.filial = 0;
+        this.uni = 0;
+        this.mes = 0;
+        this.promo = null;
+    }
+
+    public Venda(Venda v) {
+        this.codP = v.getCodP();
+        this.codC = v.getCodC();
+        this.preco = v.getPreco();
+        this.filial = v.getFilial();
+        this.uni = v.getUni();
+        this.mes = v.getMes();
+        this.promo = v.getPromo();
+    }
+
+    public String getCodP() {
+        return codP;
+    }
+
+    public String getCodC() {
+        return codC;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getFilial() {
+        return filial;
+    }
+
+    public int getUni() {
+        return uni;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public String getPromo() {
+        return promo;
+    }
+    
+    public boolean equals(Object o) {
+        
+        if (this == o) {
+            return true;
+        }
+
+        if ((o == null) || (this.getClass() != o.getClass())) {
+            return false;
+        } else {
+            Venda v = (Venda) o;
+            return (this.getCodP().equals(v.getCodP()) && this.getCodC().equals(v.getCodC()) && this.getPreco() == (v.getPreco())
+                    && this.getFilial() == (v.getFilial()) && this.getUni() == (v.getUni()) && this.getMes() == (v.getMes()) && this.getPromo().equals(v.getPromo()));
+        }
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder("-----Venda-----\n");
+
+        s.append(" Código do Produto:" + this.getCodP() + "\n");
+        s.append(" Código do Cliente:" + this.getCodC() + "\n");
+        s.append(" Filial:" + this.getFilial() + "\n");
+        s.append(" Preço:" + this.getPreco() + "\n");
+        s.append(" Promoção(P) ou Normal(N):" + this.getPromo()+"\n");
+        s.append(" Unidades vendidas:" + this.getUni() + "\n");
+        s.append(" Mês da Venda:" + this.getMes() + "\n");
+
+        return s.toString();
+    }
+
+    public Venda Clone() {
+        return new Venda(this);
+    }
+    
+   
+   public static Venda parseLinhaVenda(String linha) {
+        Venda v = new Venda();
+        int i = 0; 
+        while (linha != null){
+         for(String s: linha.split(" ",6))
+         switch (i) {
+                    case 0: 
+                        codP = s;
+                        break;
+                    case 1: 
+                        preco = Double.parseDouble(s);
+                        break;
+                    case 2: 
+                        uni = Integer.parseInt(s);
+                        break;
+                    case 3: 
+                        promo = promo;
+                        break;
+                    case 4: 
+                        codC = codC;
+                        break;
+                    case 5: 
+                        mes = Integer.parseInt(s);
+                        break;
+                    case 6: 
+                        filial = Integer.parseInt(s);
+                        break;
+                }
+        i++;
+       
+        }
+        return v;
+    }
+/*
+    public static ArrayList<Venda> parseAllLinhas(ArrayList<String> linhas) {
+        ArrayList<Venda> aux = new ArrayList<Venda>();
+
+        return aux;
+    }*/
+
+}
