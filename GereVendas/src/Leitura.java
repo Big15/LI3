0567d2d95092ff5dfa78045c209122ba51372954
Big15 <15.js.tqar@gmail.com>
2016-05-19 23:05:33 +0100

    Revert "Added files via upload"
    This reverts commit cc2cde918f75a882a134b1538c0bd025b2569f6c.

--- GereVendas/src/Leitura.java
+++ GereVendas/src/Leitura.java
@@ -1,40 +1,115 @@
 
 import java.io.*;
-import static java.lang.System.out;
 import java.util.*;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author João
+ */
 public class Leitura {
+    
 
-    public static void print(TreeSet<String> cenas) {
-        TreeSet<String> aux = cenas;
-
-        for (String s : aux) {
-            System.out.println(s);
-        }
+public static void print (TreeSet<String> cenas){
+    TreeSet<String> aux = cenas;
+   
+    for(String s: aux){
+    System.out.println(s);
     }
+    System.out.println("Número Total de Elementos:");
+    System.out.println(aux.size());
 
-    public static TreeSet<String> lerFiles(String file_path) {
-        String path;
-        TreeSet<String> prod = new TreeSet<String>();
-        path = file_path;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(path));
-
-            String cod;
-            while ((cod = reader.readLine()) != null) {
-                Crono.start();
-                prod.add(cod);
-                Crono.stop();
-            }
-        } catch (IOException e) {
-            out.println(e.getMessage());
+}
+    
+public static TreeSet<String> lerProd(String file_path){
+    String path;
+    TreeSet<String> prod =  new TreeSet<String>();
+    path = file_path;
+    try{
+    BufferedReader reader = new BufferedReader(new FileReader(path));
+   
+    String cod;
+    while((cod=reader.readLine())!= null){
+    
+    prod.add(cod);
+    
+    }
+    return prod;
+    }catch (Exception e){
+    System.err.format("IOException: %s%n", e);
             return null;
-        }
-        System.out.println("Ficheiro Vendas_3M (Buffer)");
-        System.out.println("Lidas e guardadas " + prod.size() + " linhas");
-        System.out.println("Tempo: " + Crono.print() + " segundos");
-        return prod;
     }
 
+    
+
+}
+/*
+public static TreeSet<String> lerProd2(String file_path){
+    String path;
+    String cod;
+    TreeSet<String> prod =  new TreeSet<String>();
+    path = file_path;
+      try {
+          Scanner input = new Scanner(path);
+       
+    while (input.hasNext()){
+        cod = Input.lerString();
+        prod.add(cod);
+    }    
+      
+        return prod;
+      }catch (Exception e) {
+          System.err.format("IOException: %s%n", e);
+            return null;
+    }
+    
+
+}*/
+
+public static TreeSet<String> lerClie(String file_path){
+    String path;
+    TreeSet<String> clie =  new TreeSet<String>();
+    path = file_path;
+    try{
+    BufferedReader reader = new BufferedReader(new FileReader(path));
+    String cod;
+    while((cod=reader.readLine())!= null){
+    
+       clie.add(cod);
+    
+    }
+    return clie;
+    }catch (Exception e){
+    System.err.format("IOException: %s%n", e);
+            return null;
+    }
+
+}
+
+public static TreeSet<String> lerVen(String file_path){
+    String path;
+    TreeSet<String> ven =  new TreeSet<String>();
+    path = file_path;
+    try{
+    BufferedReader reader = new BufferedReader(new FileReader(path));
+    String cod;
+    while((cod=reader.readLine())!= null){
+    
+    ven.add(cod);
+    
+    }
+    return ven;
+    }catch (Exception e){
+    System.err.format("IOException: %s%n", e);
+            return null;
+    }
+
+}
+    
 }
   
